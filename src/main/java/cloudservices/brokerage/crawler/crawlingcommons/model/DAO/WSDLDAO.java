bf3152298234d89855397b3a3db2c7b57c562387
Arash <Arash@172.30.33.83>
2014-05-28 20:59:31 -0400

    URLExists added in WSDLDAO

--- src/main/java/cloudservices/brokerage/crawler/crawlingcommons/model/DAO/WSDLDAO.java
+++ src/main/java/cloudservices/brokerage/crawler/crawlingcommons/model/DAO/WSDLDAO.java
@@ -5,15 +5,27 @@
 package cloudservices.brokerage.crawler.crawlingcommons.model.DAO;
 
 import cloudservices.brokerage.crawler.crawlingcommons.model.entities.WSDL;
-
+import java.util.List;
+import org.hibernate.Query;
 
 /**
  *
-* @author Arash Khodadadi http://www.arashkhodadadi.com/  
+ * @author Arash Khodadadi http://www.arashkhodadadi.com/
  */
-public class WSDLDAO extends BaseDAO{
-    
-    public void addWSDL(WSDL wsdl) throws DAOException{
+public class WSDLDAO extends BaseDAO {
+
+    public void addWSDL(WSDL wsdl) throws DAOException {
         super.save(wsdl);
+    }
+
+    public boolean URLExists(String url) throws DAOException {
+        Query query = super.getSession().createQuery("from WSDL where url = :para1 ");
+        query.setParameter("para1", url);
+        List list = query.list();
+        if (list.isEmpty()) {
+            return false;
+        } else {
+            return true;
+        }
     }
 }
