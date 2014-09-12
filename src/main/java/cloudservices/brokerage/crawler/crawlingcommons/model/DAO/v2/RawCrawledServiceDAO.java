335b9372dc5162519a9620be548b0df14208534f
Arash <Arash@172.30.33.83>
2014-09-11 22:30:56 -0400

    find by url added

--- src/main/java/cloudservices/brokerage/crawler/crawlingcommons/model/DAO/v2/RawCrawledServiceDAO.java
+++ src/main/java/cloudservices/brokerage/crawler/crawlingcommons/model/DAO/v2/RawCrawledServiceDAO.java
@@ -7,6 +7,8 @@ package cloudservices.brokerage.crawler.crawlingcommons.model.DAO.v2;
 import cloudservices.brokerage.crawler.crawlingcommons.model.DAO.BaseDAO;
 import cloudservices.brokerage.crawler.crawlingcommons.model.DAO.DAOException;
 import cloudservices.brokerage.crawler.crawlingcommons.model.entities.v2.RawCrawledService;
+import java.util.List;
+import org.hibernate.Query;
 
 /**
  *
@@ -16,5 +18,18 @@ public class RawCrawledServiceDAO extends BaseDAO {
 
     public void addCrawledService(RawCrawledService crawledService) throws DAOException {
         super.save(crawledService);
+    }
+    
+    public RawCrawledService findByUrl(String url) throws DAOException {
+        Query query = super.getSession().createQuery("from RawCrawledService where url = :para1 ");
+        query.setParameter("para1", url);
+        List list = query.list();
+        if (list.isEmpty()) {
+            return null;
+        } else if (list.size() == 1) {
+            return (RawCrawledService) list.get(0);
+        } else {
+            throw new DAOException("Duplicate tuples for url = " + url);
+        }
     }
 }
