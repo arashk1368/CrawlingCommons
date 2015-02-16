2d4116ff20e54d9af738c4038e520f2a92b83f79
Arash Khodadadi <arashk1368@gmail.com>
2015-02-15 20:44:11 -0500

    service provider DAO implemented

--- src/main/java/cloudservices/brokerage/crawler/crawlingcommons/model/DAO/v3/ServiceProviderDAO.java
+++ src/main/java/cloudservices/brokerage/crawler/crawlingcommons/model/DAO/v3/ServiceProviderDAO.java
@@ -4,10 +4,31 @@
  */
 package cloudservices.brokerage.crawler.crawlingcommons.model.DAO.v3;
 
+import cloudservices.brokerage.crawler.crawlingcommons.model.DAO.BaseDAO;
+import cloudservices.brokerage.crawler.crawlingcommons.model.DAO.DAOException;
+import cloudservices.brokerage.crawler.crawlingcommons.model.entities.v3.ServiceProvider;
+import java.util.List;
+import org.hibernate.Query;
+
 /**
  *
  * @author Arash Khodadadi <http://www.arashkhodadadi.com/>
  */
-public class ServiceProviderDAO {
-    
+public class ServiceProviderDAO extends BaseDAO {
+    public void addServiceProvider(ServiceProvider serviceProvider) throws DAOException {
+        super.save(serviceProvider);
+    }
+
+    public ServiceProvider findByUrl(String url) throws DAOException {
+        Query query = super.getSession().createQuery("from ServiceProvider where url = :para1 ");
+        query.setParameter("para1", url);
+        List list = query.list();
+        if (list.isEmpty()) {
+            return null;
+        } else if (list.size() == 1) {
+            return (ServiceProvider) list.get(0);
+        } else {
+            throw new DAOException("Duplicate tuples for url = " + url);
+        }
+    }
 }
