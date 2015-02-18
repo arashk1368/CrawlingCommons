ad77d190a526c42c34e7237dac4357451646641e
Arash Khodadadi <arashk1368@gmail.com>
2015-02-18 02:11:15 -0500

    get methods with different strategy queries for services added

--- src/main/java/cloudservices/brokerage/crawler/crawlingcommons/model/DAO/v3/ServiceDescriptionDAO.java
+++ src/main/java/cloudservices/brokerage/crawler/crawlingcommons/model/DAO/v3/ServiceDescriptionDAO.java
@@ -7,6 +7,7 @@ package cloudservices.brokerage.crawler.crawlingcommons.model.DAO.v3;
 import cloudservices.brokerage.crawler.crawlingcommons.model.DAO.BaseDAO;
 import cloudservices.brokerage.crawler.crawlingcommons.model.DAO.DAOException;
 import cloudservices.brokerage.crawler.crawlingcommons.model.entities.v3.ServiceDescription;
+import cloudservices.brokerage.crawler.crawlingcommons.model.enums.v3.ServiceDescriptionType;
 import java.util.List;
 import org.hibernate.Query;
 
@@ -37,4 +38,37 @@ public class ServiceDescriptionDAO extends BaseDAO {
         Query query = super.getSession().createQuery("from ServiceDescription where serviceProvider is null");
         return query.list();
     }
+
+    public List<ServiceDescription> getAllWithType(ServiceDescriptionType type) throws DAOException {
+        Query query = super.getSession().createQuery("from ServiceDescription where type = :para1");
+        query.setParameter("para1", type);
+        return query.list();
+    }
+
+    public List<ServiceDescription> getBothTimesNull(ServiceDescriptionType type) throws DAOException {
+        Query query = super.getSession().createQuery("from ServiceDescription where type = :para1 and lastAvailableTime is null and lastUnavailableTime is null");
+        query.setParameter("para1", type);
+        return query.list();
+    }
+
+    public List<ServiceDescription> getNeverAvailable(ServiceDescriptionType type) throws DAOException {
+        Query query = super.getSession().createQuery("from ServiceDescription where type = :para1 and lastAvailableTime is null");
+        query.setParameter("para1", type);
+        return query.list();
+    }
+
+    public List<ServiceDescription> getUpdated(ServiceDescriptionType type) throws DAOException {
+        Query query = super.getSession().createQuery("from ServiceDescription where type = :para1 and updated = :para2");
+        query.setParameter("para1", type);
+        query.setParameter("para2", true);
+        return query.list();
+    }
+
+    public List<ServiceDescription> getUnavailable(ServiceDescriptionType type) throws DAOException {
+        Query query = super.getSession().createQuery("from ServiceDescription where type = :para1 and wasAvailable = :para2");
+        query.setParameter("para1", type);
+        query.setParameter("para2", false);
+        return query.list();
+    }
+
 }
