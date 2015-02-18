97945caeb10923f6829134ed471c160c1b168640
Arash Khodadadi <arashk1368@gmail.com>
2015-02-18 02:10:55 -0500

    gettin last in snapshot dao aded

--- src/main/java/cloudservices/brokerage/crawler/crawlingcommons/model/DAO/v3/ServiceDescriptionSnapshotDAO.java
+++ src/main/java/cloudservices/brokerage/crawler/crawlingcommons/model/DAO/v3/ServiceDescriptionSnapshotDAO.java
@@ -6,7 +6,11 @@ package cloudservices.brokerage.crawler.crawlingcommons.model.DAO.v3;
 
 import cloudservices.brokerage.crawler.crawlingcommons.model.DAO.BaseDAO;
 import cloudservices.brokerage.crawler.crawlingcommons.model.DAO.DAOException;
+import cloudservices.brokerage.crawler.crawlingcommons.model.entities.v3.ServiceDescription;
 import cloudservices.brokerage.crawler.crawlingcommons.model.entities.v3.ServiceDescriptionSnapshot;
+import cloudservices.brokerage.crawler.crawlingcommons.model.enums.v3.ServiceDescriptionType;
+import java.util.List;
+import org.hibernate.Query;
 
 /**
  *
@@ -16,5 +20,17 @@ public class ServiceDescriptionSnapshotDAO extends BaseDAO {
 
     public void addSnapshot(ServiceDescriptionSnapshot snapshot) throws DAOException {
         super.save(snapshot);
+    }
+
+    public ServiceDescriptionSnapshot getLast(ServiceDescription description, ServiceDescriptionType type) throws DAOException {
+        Query query = super.getSession().createQuery("from ServiceDescriptionSnapshot where type = :para1 and serviceDescription = :para2 order by accessedTime desc");
+        query.setParameter("para1", type);
+        query.setParameter("para2", description);
+        List list = query.list();
+        if (list.isEmpty()) {
+            return null;
+        } else {
+            return (ServiceDescriptionSnapshot) list.get(0);
+        }
     }
 }
